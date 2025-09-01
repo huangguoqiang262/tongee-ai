78deac80693c434b6929842d36afb5c282ebc699
黄国强 <1017827480@qq.com>
2025-09-01 11:44:19 +0800

    ceshi

--- src/main/index.js
+++ src/main/index.js
@@ -161,6 +161,3 @@ app.on('before-quit', () => {
     tray.destroy()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
