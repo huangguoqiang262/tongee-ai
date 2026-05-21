6e8f45dde8696a07410c5301f3faa8e3218125bc
黄国强 <1017827480@qq.com>
2026-05-21 09:17:20 +0800

    修改域名

--- src/renderer/src/components/layout/Sidebar.vue
+++ src/renderer/src/components/layout/Sidebar.vue
@@ -105,7 +105,7 @@ const handleClick = (item) => {
   }
   if (item.url == 'Management') {
     addNewTab({
-      url: 'https://tangji.baguatan.cn?t=' + Date.now(),
+      url: 'http://192.168.31.187:3000?t=' + Date.now(),
       title: item.name,
       icon: item.icon,
       isInternal: false
