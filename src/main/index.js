eccae3895d2f8f8d3314094de69f3ebbf5161813
黄国强 <1017827480@qq.com>
2026-06-12 16:43:47 +0800

    开发及优化客户需求

--- src/main/index.js
+++ src/main/index.js
@@ -213,7 +213,7 @@ app.whenReady().then(() => {
 
   autoUpdater.setFeedURL({
     provider: 'generic',
-    url: 'http://192.168.31.187:3000/updates',
+    url: 'http://192.168.11.241/updates',
     channel: 'latest' // 明确指定更新通道
   })
   autoUpdater.requestHeaders = { insecure: 'true' } // 跳过证书验证
